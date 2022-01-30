2e4f94fc2f295382400479f169ec678c6734ce4e
Filip Tijan <ftijan@gmail.com>
2022-01-30 02:14:37 +0000

    material dialog template, bindings, methods

--- src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-new-contact-dialog',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-contact-dialog.component.scss']
 })
 export class NewContactDialogComponent implements OnInit {
+  avatars = [
+    'svg-1',
+    'svg-2',
+    'svg-3',
+    'svg-4',
+  ];
 
-  constructor() { }
+  user!: User;
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
 
   ngOnInit(): void {
+    this.user = new User();
   }
 
+  save() {
+    this.dialogRef.close(this.user);
+  }
+
+  dismiss() {
+    this.dialogRef.close(null);
+  }
 }
--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -16,9 +16,12 @@ export class ToolbarComponent implements OnInit {
   }
 
   openAddContactDialog(): void {
-    this.dialog.open(NewContactDialogComponent, {
+    let dialogRef = this.dialog.open(NewContactDialogComponent, {
       width: "450px"
     });
-  }
 
+    dialogRef.afterClosed().subscribe({
+      next: result => console.log(result)
+    });
+  }
 }
