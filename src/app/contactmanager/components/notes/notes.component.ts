450dc1653c75607853f7fb78667223d3425228de
Filip Tijan <ftijan@gmail.com>
2022-01-30 00:32:04 +0000

    material data table with data source

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
 @Component({
@@ -8,10 +9,14 @@ import { Note } from '../../models/note';
 })
 export class NotesComponent implements OnInit {
   @Input() notes: Note[] = [];
+  
+  displayedColumns: string[] = ['id', 'title', 'date'];
+  dataSource!: MatTableDataSource<Note>;  
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
 }
